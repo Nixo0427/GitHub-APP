68cb4e977abf6f1718c775c9030ab968c13e15f5
Nixo0427 <Nixo1649883744@Gmail.com>
2018-11-06 19:48:15 +0800

    数据拿到回调，找时间去把仓库界面搭好

--- app/src/main/java/github/nixo/com/Common/NetWork/Repository/RepositoryApi.kt
+++ app/src/main/java/github/nixo/com/Common/NetWork/Repository/RepositoryApi.kt
@@ -1,7 +1,6 @@
 package github.nixo.com.Common.NetWork.Repository
 
 import github.nixo.com.github.NetWork.retrofit
-import io.reactivex.Observable
 import retrofit2.adapter.rxjava.GitHubPaging
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -9,8 +8,7 @@ import retrofit2.http.Query
 
 interface RepositoryApi{
 
-    @GET("/user/{owner}/repos?type=all")
-    fun listRepositoriesOfUser(@Path("owner") owner: String ,@Query("page")page : Int = 1 , @Query("per_page")perpage:Int = 20): Observable<GitHubPaging<Repository>>
-
+    @GET("/users/{owner}/repos?type=all")
+    fun listRepositoriesOfUser(@Path("owner") owner: String, @Query("page") page: Int = 1, @Query("per_page") per_page: Int = 20): rx.Observable<GitHubPaging<Repository>>
 }
 object RepositoryService : RepositoryApi by retrofit.create(RepositoryApi::class.java)
